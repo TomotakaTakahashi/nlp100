669eefc1cfefd0f8aa987fb55bc4ed27bcc59d34
TomotakaTakahashi <domefasoltedo@gmail.com>
2016-08-12 01:25:57 +0900

    remove ',' and '.'

--- 03.py
+++ 03.py
@@ -1,4 +1,6 @@
 s="Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+s=s.replace(',','')
+s=s.replace('.','')
 ss=s.split(' ')
 res = []
 for word in ss:
